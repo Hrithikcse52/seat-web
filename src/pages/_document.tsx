6caf4db0181b6351e8ddff5d8c81fabfb35f0ee0
hrithikcse52 <hrithik.cse52@gmail.com>
2022-05-23 13:27:56 +0530

    analytics adde

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="ff7f7cbc-5f7f-4669-a1ec-fca956437b21"
+          src="https://umami-production-716c.up.railway.app/umami.js"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
