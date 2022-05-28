ffc82777e05ec9337d8a5dbfb42aa46900a16ee8
hrithikcse52 <hrithik.cse52@gmail.com>
2022-05-29 01:02:41 +0530

    ref: trending retrived and skeletons imporoved

--- src/components/feed/feed.comp.tsx
+++ src/components/feed/feed.comp.tsx
@@ -110,37 +110,51 @@ export default function FeedPosts() {
               </div>
             </div>
           )}
-          {isLoading && !isPostFetched && (
-            <div className="flex w-full p-8 border-b border-gray-300">
-              <Skeleton height={50} circle mb="xl" />
+          {isLoading &&
+            !isPostFetched &&
+            ["1", "2", "3", "4", "5"].map(id => (
+              <div key={id} className="flex w-full p-8 border-b border-gray-300">
+                <Skeleton height={50} circle mb="xl" />
 
-              <div className="flex flex-col flex-grow ml-4">
-                <div className="flex">
-                  {/* <span className="font-semibold">Username</span>
-                  <span className="ml-1">@username</span>
-                  <span className="ml-auto text-sm">Just now</span> */}
-                </div>
-                <p className="mt-1">
-                  <Skeleton height={8} mt={6} radius="xl" />
-
-                  {/* <a className="underline" href="#">
-                    
-                  </a> */}
-                </p>
-                <div className="flex mt-2">
-                  <button type="button" className="text-sm font-semibold">
-                    Like
-                  </button>
-                  <button type="button" className="ml-2 text-sm font-semibold">
-                    Reply
-                  </button>
-                  <button type="button" className="ml-2 text-sm font-semibold">
-                    Share
-                  </button>
+                <div className="flex flex-col flex-grow ml-4">
+                  <div className="mt-1">
+                    <Skeleton height={8} mt={6} radius="xl" />
+                  </div>
+                  <div className="mt-4 w-full flex items-center justify-between">
+                    <div className="flex items-center">
+                      <button type="button" className="flex items-center group mr-8">
+                        <div className="border border-gray-gray3 bg-white hover:bg-gray-gray2 cursor-pointer rounded-full flex justify-center items-center transition-all ease-in duration-75 lg:w-8 lg:h-8 w-6 h-6 border-none group-hover:bg-green-lighter group-hover:text-green-bright false">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                          >
+                            <path d="M21.3,10.08A3,3,0,0,0,19,9H14.44L15,7.57A4.13,4.13,0,0,0,11.11,2a1,1,0,0,0-.91.59L7.35,9H5a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H17.73a3,3,0,0,0,2.95-2.46l1.27-7A3,3,0,0,0,21.3,10.08ZM7,20H5a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H7Zm13-7.82-1.27,7a1,1,0,0,1-1,.82H9V10.21l2.72-6.12A2.11,2.11,0,0,1,13.1,6.87L12.57,8.3A2,2,0,0,0,14.44,11H19a1,1,0,0,1,.77.36A1,1,0,0,1,20,12.18Z" />
+                          </svg>
+                        </div>
+                        <span className="pl-1 text-sm font-normal">1</span>
+                      </button>
+                      <button type="button" className="flex items-center group ">
+                        <div className="border border-gray-gray3 bg-white hover:bg-gray-gray2 cursor-pointer rounded-full flex justify-center items-center transition-all ease-in duration-75 lg:w-8 lg:h-8 w-6 h-6 border-none group-hover:bg-green-lighter group-hover:text-green-bright ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                          >
+                            <path d="M13,11H7a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2Zm4-4H7A1,1,0,0,0,7,9H17a1,1,0,0,0,0-2Zm2-5H5A3,3,0,0,0,2,5V15a3,3,0,0,0,3,3H16.59l3.7,3.71A1,1,0,0,0,21,22a.84.84,0,0,0,.38-.08A1,1,0,0,0,22,21V5A3,3,0,0,0,19,2Zm1,16.59-2.29-2.3A1,1,0,0,0,17,16H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4H19a1,1,0,0,1,1,1Z" />
+                          </svg>
+                        </div>
+                        <span className="pl-1 text-sm font-normal" />
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            ))}
           {isPostFetched &&
             posts &&
             posts.map(post => (
@@ -239,6 +253,54 @@ export default function FeedPosts() {
               </div>
             </div>
           </div> */}
+        </div>
+      </div>
+      <div className="flex flex-col flex-shrink-0 w-1/4 py-4 pl-4">
+        <h3 className="mt-6 font-semibold">Trending</h3>
+        <div className="flex w-full py-4 border-b border-gray-300">
+          <span className="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full" />
+          <div className="flex flex-col flex-grow ml-2">
+            <div className="flex text-sm">
+              <span className="font-semibold">Username</span>
+              <span className="ml-1">@username</span>
+            </div>
+            <p className="mt-1 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, et dolore magna aliqua.{" "}
+              <a className="underline" href="#">
+                #hashtag
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full py-4 border-b border-gray-300">
+          <span className="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full" />
+          <div className="flex flex-col flex-grow ml-2">
+            <div className="flex text-sm">
+              <span className="font-semibold">Username</span>
+              <span className="ml-1">@username</span>
+            </div>
+            <p className="mt-1 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, et dolore magna aliqua.{" "}
+              <a className="underline" href="#">
+                #hashtag
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full py-4 border-b border-gray-300">
+          <span className="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full" />
+          <div className="flex flex-col flex-grow ml-2">
+            <div className="flex text-sm">
+              <span className="font-semibold">Username</span>
+              <span className="ml-1">@username</span>
+            </div>
+            <p className="mt-1 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, et dolore magna aliqua.{" "}
+              <a className="underline" href="#">
+                #hashtag
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
