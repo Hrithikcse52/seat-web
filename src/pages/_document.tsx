bd40015edd1d7c283e33bf587abb18ccca382dd6
hrithikcse52 <hrithik.cse52@gmail.com>
2022-05-29 09:28:05 +0530

    feat: GA analytics added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -11,6 +12,16 @@ export default function Document() {
           data-website-id="ccf915e6-8cea-4e99-b0f8-ecce7039fa40"
           src="https://umami-production-716c.up.railway.app/umami.js"
         />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-421B10J8PL" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-421B10J8PL');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
